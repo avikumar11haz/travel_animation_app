f4b2df72a44baa430227b4e47f756d7a8619dde0
unknown <avikumarr355@gmail.com>
2024-01-17 15:07:10 +0530

    add rawmaterial button and extract a widget

--- lib/home.dart
+++ lib/home.dart
@@ -9,15 +9,59 @@ class Home extends StatelessWidget {
     return Scaffold(
       body: Container(
         height: MediaQuery.of(context).size.height,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter, end: Alignment.bottomCenter,
-            colors: [
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
               AppColor.secondaryColor,
               AppColor.tertiaryColor,
-            ]
-          )
+            ])),
+        child: ListView(
+          children: const [
+            Padding(
+              padding:
+                  EdgeInsets.only(left: 25, right: 25, bottom: 50, top: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  PrimaryText(),
+                  RawMaterialButton(
+                    constraints: BoxConstraints(minWidth: 0),
+                    onPressed: null,
+                    elevation: 2.0,
+                    padding: EdgeInsets.all(8),
+                    shape: CircleBorder(),
+                    fillColor: Colors.white30,
+                    child: Icon(
+                      Icons.search_rounded,
+                      color: AppColor.primaryColor,
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class PrimaryText extends StatelessWidget {
+  const PrimaryText({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "Destination",
+      style: TextStyle(
+        fontFamily: 'Roboto',
+        fontSize: 32,
+        color: AppColor.primaryColor,
+        fontWeight: FontWeight.w700,
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app_ui/home.dart';
 
 void main() {
   runApp(const MyApp());
